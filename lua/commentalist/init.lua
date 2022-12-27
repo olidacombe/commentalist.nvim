bf0540f41a6b5cc51db3b7a6a6de862b8ce12de7
Oli Dacombe <odacombe@blockchain.com>
2022-12-27 23:20:52 +0200

    Pass range to figlet

--- lua/commentalist/init.lua
+++ lua/commentalist/init.lua
@@ -22,21 +22,26 @@ local figlet = function(string, font, callback)
         table.insert(cmdline_args, "-f")
         table.insert(cmdline_args, font)
     end
+    table.insert(cmdline_args, "--")
     table.insert(cmdline_args, string)
     shell_render_job(cmdline_args, callback)
 end
 
 M.comment = function(opts)
     --P(opts)
+    local line1, line2 = opts.line1, opts.line2
+    local buf = vim.api.nvim_get_current_buf()
+    -- nvi_buf_get_lines Indexing is zero-based, end-exclusive.
+    local lines = vim.api.nvim_buf_get_lines(buf, line1 - 1, line2, false)
+    lines = table.concat(lines, "\n")
+    P(lines)
     -- TODO offer all renderers, not just figlet
-    figlet("bla", opts.fargs[1], function(output)
-        local buf = vim.api.nvim_get_current_buf()
-        vim.api.nvim_buf_set_lines(buf, opts.line1, opts.line2, false, output)
+    figlet(lines, opts.fargs[1], function(output)
+        vim.api.nvim_buf_set_lines(buf, line1, line2, false, output)
     end)
 end
 
 return M
-
 
 -- hey
 -- hello
