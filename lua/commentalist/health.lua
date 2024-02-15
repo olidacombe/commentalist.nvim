a4ae14db8e25ccb7e67927013605f24c01f5a9a3
olidacombe <olidacombe@gmail.com>
2024-02-15 23:05:10 +0000

    update deprecated `vim.health.report_start` (#9)
    * update deprecated `vim.health.report_start`
    
    * update deprecated `vim.health.report_*` usage

--- lua/commentalist/health.lua
+++ lua/commentalist/health.lua
@@ -1,4 +1,4 @@
-local health = vim.health or require "health"
+local health = vim.health or require("health")
 table.unpack = table.unpack or unpack
 
 local M = {}
@@ -7,90 +7,90 @@ local M = {}
 --     binary_name = { ... additional required binaries }
 -- }
 local optional_binaries = {
-    -- boxes
-    boxes = {},
-    -- cowsay
-    cowsay = {},
-    -- figlet: requires also `figlist` for getting fonts
-    figlet = { "figlist" },
+	-- boxes
+	boxes = {},
+	-- cowsay
+	cowsay = {},
+	-- figlet: requires also `figlist` for getting fonts
+	figlet = { "figlist" },
 }
 
 -- check whether all binaries are found, returning a global
 -- boolean and individual booleans per binary
 local check_binaries = function(bins)
-    local ok = true
-    local findings = {}
-    for _, bin in ipairs(bins) do
-        if vim.fn.executable(bin) == 1 then
-            findings[bin] = true
-        else
-            findings[bin] = false
-            ok = false
-        end
-    end
-    return ok, findings
+	local ok = true
+	local findings = {}
+	for _, bin in ipairs(bins) do
+		if vim.fn.executable(bin) == 1 then
+			findings[bin] = true
+		else
+			findings[bin] = false
+			ok = false
+		end
+	end
+	return ok, findings
 end
 
 -- simply return bool for whether or not `bin` and all extras
 -- for that binary are found
 M.check_binaries = function(bin)
-    local binaries = { bin, table.unpack(optional_binaries[bin] or {}) }
-    local ok, _ = check_binaries(binaries)
-    return ok
+	local binaries = { bin, table.unpack(optional_binaries[bin] or {}) }
+	local ok, _ = check_binaries(binaries)
+	return ok
 end
 
 -- return which optional binary packages have been found present
 M.installed_packages = function()
-    local packages = {}
-    for bin, _ in pairs(optional_binaries) do
-        if M.check_binaries(bin) then
-            table.insert(packages, bin)
-        end
-    end
-    return packages
+	local packages = {}
+	for bin, _ in pairs(optional_binaries) do
+		if M.check_binaries(bin) then
+			table.insert(packages, bin)
+		end
+	end
+	return packages
 end
 
 -- use check_binaries for all in `optional_binaries` to generate
 -- a healthcheck report
 local bin_report = function()
-    for bin, extra in pairs(optional_binaries) do
-        local _, findings = check_binaries({ bin, table.unpack(extra) })
-        for bin, found in pairs(findings) do
-            if found then
-                health.report_ok(("`%s` found"):format(bin))
-            else
-                health.report_warn(("`%s` not found"):format(bin))
-            end
-        end
-    end
+	for bin, extra in pairs(optional_binaries) do
+		local _, findings = check_binaries({ bin, table.unpack(extra) })
+		for bin, found in pairs(findings) do
+			if found then
+				health.ok(("`%s` found"):format(bin))
+			else
+				health.warn(("`%s` not found"):format(bin))
+			end
+		end
+	end
 end
 
 local function lualib_installed(lib_name)
-    local res, _ = pcall(require, lib_name)
-    return res
+	local res, _ = pcall(require, lib_name)
+	return res
 end
 
 local required_plugins = {
-    { lib = "plenary", optional = false, },
-    { lib = "telescope", optional = false, },
+	{ lib = "plenary", optional = false },
+	{ lib = "telescope", optional = false },
 }
 
 M.check = function()
-    health.report_start "Checking for required plugins"
-    for _, plugin in ipairs(required_plugins) do
-        if lualib_installed(plugin.lib) then
-            health.report_ok(plugin.lib .. " installed.")
-        else
-            local lib_not_installed = plugin.lib .. " not found."
-            if plugin.optional then
-                health.report_warn(("%s %s"):format(lib_not_installed, plugin.info))
-            else
-                health.report_error(lib_not_installed)
-            end
-        end
-    end
-    health.report_start "Checking for default renderers"
-    bin_report()
+	health.start("Checking for required plugins")
+	for _, plugin in ipairs(required_plugins) do
+		if lualib_installed(plugin.lib) then
+			health.ok(plugin.lib .. " installed.")
+		else
+			local lib_not_installed = plugin.lib .. " not found."
+			if plugin.optional then
+				health.warn(("%s %s"):format(lib_not_installed, plugin.info))
+			else
+				health.error(lib_not_installed)
+			end
+		end
+	end
+	health.start("Checking for default renderers")
+	bin_report()
 end
 
 return M
